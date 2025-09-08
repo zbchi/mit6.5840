ce7107080014556b25c9bb001e0c5354ac04ef8b
zb <1662308219@qq.com>
2025-09-08 11:18:13 +0800

    feat(lock): add Acquire and Release methods for distributed lock

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,11 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"fmt"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -9,8 +13,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
-	// You may add code here
+	ck   kvtest.IKVClerk
+	name string
+	me   string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +24,49 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{ck: ck, name: l, me: kvtest.RandValue(8)}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	i := 0
+	for {
+		value, version, err := lk.ck.Get(lk.name)
+		fmt.Printf("[%s,%s] Acquire try %d, value: %s\n", lk.me, lk.name, i, value)
+		if err == rpc.ErrNoKey { // free lock
+			lk.ck.Put(lk.name, lk.me, 0)
+			fmt.Printf("[%s,%s] Acquire success (no key)\n", lk.me, lk.name)
+			break
+		} else if value == "" { // free lock
+			err := lk.ck.Put(lk.name, lk.me, version)
+			if err != rpc.OK {
+				fmt.Printf("[%s,%s] Acquire put failed: %v\n", lk.me, lk.name, err)
+			} else {
+				fmt.Printf("[%s,%s] Acquire success\n", lk.me, lk.name)
+				break
+			}
+		} //retry
+		time.Sleep(50 * time.Millisecond)
+
+		i++
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	fmt.Printf("[%s,%s] Release start\n", lk.me, lk.name)
+	value, version, err := lk.ck.Get(lk.name)
+	if err == rpc.ErrNoKey {
+		fmt.Printf("[%s,%s] Release failed: ErrNoKey\n", lk.me, lk.name)
+		return
+	} else if value != lk.me {
+		fmt.Printf("[%s,%s] Release failed: not owner\n", lk.me, lk.name)
+		return
+	} else if value == lk.me {
+		err := lk.ck.Put(lk.name, "", version)
+		if err != rpc.OK {
+			fmt.Printf("[%s,%s] Release put failed: %v\n", lk.me, lk.name, err)
+		} else {
+			fmt.Printf("[%s,%s] Release success\n", lk.me, lk.name)
+		}
+	}
 }
